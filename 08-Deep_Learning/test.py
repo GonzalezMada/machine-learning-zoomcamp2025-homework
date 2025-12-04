d7b49bdd23c9901b795369ef2d9c7c06ad9c4efa
Gonzalez Harilanto ANDRINIRINA <122888447+GonzalezMada@users.noreply.github.com>
2025-12-04 11:53:14 +0000

    Testing the TF Model

---
+++ 08-Deep_Learning/test.py
@@ -0,0 +1,22 @@
+import numpy as np
+import os
+import tensorflow as tf
+from tensorflow import keras
+
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+
+print(os.getcwd())
+model = keras.models.load_model('/workspaces/machine-learning-zoomcamp2025-homework/08-Deep_Learning/clothes_classification_model.h5')
+
+
+image_path = '/workspaces/machine-learning-zoomcamp2025-homework/08-Deep_Learning/image_t-shirt.jpg'
+img = load_img(image_path, target_size=(150, 150))
+img_array = img_to_array(img)
+img_array = np.expand_dims(img_array, axis=0)
+
+predictions = model.predict(img_array)
+predicted_class = np.argmax(predictions, axis=1)
+labels = ['dress', 'hat', 'longsleeve', 'outwear', 'pants', 'shirt', 'shoes', 'shorts', 'skirt', 't-shirt']
+print(f'The predicted class is: {labels[predicted_class[0]]}')
+
+
